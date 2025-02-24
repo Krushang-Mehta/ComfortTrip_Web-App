3d345ed9ec81a92d9c9d9730705b60ed5485940a
Krushang Mehta <krushang2005@gmail.com>
2025-02-25 00:27:08 +0530

    Update page.jsx

--- src/app/Login/page.jsx
+++ src/app/Login/page.jsx
@@ -76,7 +76,7 @@ const Login = () => {
                     </form>
                     <div className="mt-6">
                         <button className="w-full border border-gray-300 flex items-center justify-center py-2 px-4 rounded-lg hover:bg-gray-100">
-                            <img
+                            <Image
                                 src="/Google.png" // Replace with an actual path to a Google logo
                                 alt="Google Logo"
                                 className="w-5 h-5 mr-2"
