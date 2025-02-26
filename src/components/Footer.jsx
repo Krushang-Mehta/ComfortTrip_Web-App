9a45ac5ecbd51ae1dd2123657583bf7c766b5f7f
Krushang Mehta <krushang2005@gmail.com>
2025-02-26 15:52:01 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -107,7 +107,7 @@ export default Footer;
 //             <div className="text-gray-600 mb-4">
 //               Shyamnagar 1,<br />
 //               Gandhigram , <br />
-//               Ramdevpir Chowk ,Ramdevpir Chowk , <br />
+//               Ramdevpir Chowk ,150 Ft Ring Road, <br />
 //               Rajkot:- 360007.
 //             </div>
 //             <div className="text-gray-600">
