8105d96ffb876a00d9614f54cbbd0e3c1bbb05a8
Krushang Mehta <krushang2005@gmail.com>
2026-03-25 17:32:24 +0530

    Fix telephone link format in Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -25,7 +25,7 @@ const Footer = () => {
              </div>
             <div className="text-gray-600 flex flex-col gap-2">
               <div className="flex items-center gap-2">
-                <a href="tel"+919974833330"><FontAwesomeIcon icon={faWhatsapp} className="text-gray-700" />
+                <a href="tel:+919974833330"><FontAwesomeIcon icon={faWhatsapp} className="text-gray-700" />
                 +91 9974833330</a>
               </div>
               <div className="flex items-center gap-2">
